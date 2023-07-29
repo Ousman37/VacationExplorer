ebcf419198d17b0cada69a12f756dab012a4a530
Ethmane Didi <ousman.didibela@gmail.com>
2023-07-29 18:33:10 +0200

    Updated UI for Logo and Search components for responsiveness

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -6,15 +6,13 @@ const Logo = () => {
   return (
     <Link href="/" passHref>
       <div
-        className="cursor-pointer font-bold tracking-wider text-xs sm:text-sm md:text-xl lg:text-3xl px-2 sm:px-3 md:px-5 lg:px-8 py-1 sm:py-3 md:py-3 lg:py-4 md:rounded-full md:shadow-md md:border-3 border-transparent"
+        className="cursor-pointer font-bold tracking-wider text-xs sm:text-sm md:text-xl lg:text-3xl px-2 sm:px-3 md:px-5 lg:px-8 py-1 sm:py-3 md:py-3 lg:py-4 "
         style={{
           fontFamily: 'Pacifico, cursive',
           textDecoration: 'none',
-          backgroundColor: '#F4A261',
+          backgroundColor: '#FFFFFF', // Set background color to white
           color: '#000033', // Dark blue color
-          textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
-          boxShadow: '5px 5px 15px 5px rgba(0, 0, 0, 0.2)',
-          border: '3px solid #F4A261',
+          textShadow: '3px 3px 6px rgba(0, 0, 0, 0.7)', // Shadow effect
         }}
       >
         <h1>Vacation Explorer</h1>
--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -49,15 +49,14 @@ const Search = () => {
 
     return 'Add Guests';
   }, [guestCount]);
-
   return (
     <div
       onClick={searchModal.onOpen}
-      className="cursor-pointer font-bold tracking-wider text-xs sm:text-sm md:text-xl lg:text-3xl px-2 sm:px-3 md:px-5 lg:px-8 py-1 sm:py-2 md:py-3 lg:py-4 rounded-none md:rounded-full shadow-md hover:shadow-lg border-3 border-transparent flex flex-col md:flex-row items-center justify-between bg-teal-700"
+      className="cursor-pointer font-bold tracking-wider text-xs sm:text-sm md:text-xl lg:text-4xl px-2 sm:px-3 md:px-5 lg:px-8 py-1 sm:py-2 md:py-3 lg:py-4 rounded-none md:rounded-full shadow-md hover:shadow-lg border-3 border-transparent flex flex-col md:flex-row items-center justify-between bg-teal-700 mx-auto sm:mx-0" // Added centering for smaller screens
       style={{
         fontFamily: 'Helvetica, sans-serif',
         textDecoration: 'none',
-        color: '#000033', // Dark blue color
+        color: '#000033',
         textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
         boxShadow: '5px 5px 15px 5px rgba(0, 0, 0, 0.2)',
       }}
@@ -70,7 +69,8 @@ const Search = () => {
       </div>
       <div className="text-xxxs sm:text-xs md:text-sm lg:text-base pl-0.5 sm:pl-2 md:pl-4 lg:pl-6 pr-2 text-white flex flex-row items-center gap-0.5 sm:gap-2 md:gap-3">
         <div>{guestLabel}</div>
-        <div className="p-0.5 sm:p-2 md:p-3 bg-soft-coral-500 rounded-full">
+        <div className="p-0.5 sm:p-2 md:p-3 bg-soft-coral-500 rounded-full flex flex-row items-center">
+          <span>Search</span> {/* Added label */}
           <BiSearch className="w-2.5  md:w-5 lg:w-6 h-2.5 sm:h-4 md:h-5 lg:h-6" />
         </div>
       </div>
@@ -79,3 +79,32 @@ const Search = () => {
 };
 
 export default Search;
+//   return (
+//     <div
+//       onClick={searchModal.onOpen}
+//       className="cursor-pointer font-bold tracking-wider text-xs sm:text-sm md:text-xl lg:text-3xl px-2 sm:px-3 md:px-5 lg:px-8 py-1 sm:py-2 md:py-3 lg:py-4 rounded-none md:rounded-full shadow-md hover:shadow-lg border-3 border-transparent flex flex-col md:flex-row items-center justify-between bg-teal-700"
+//       style={{
+//         fontFamily: 'Helvetica, sans-serif',
+//         textDecoration: 'none',
+//         color: '#000033', // Dark blue color
+//         textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+//         boxShadow: '5px 5px 15px 5px rgba(0, 0, 0, 0.2)',
+//       }}
+//     >
+//       <div className="text-xxxs sm:text-xs md:text-sm lg:text-base font-bold px-0.5 sm:px-2 md:px-4 lg:px-6 text-white">
+//         {locationLabel}
+//       </div>
+//       <div className="text-xxxs sm:text-xs md:text-sm lg:text-base font-semibold px-0.5 sm:px-2 md:px-4 lg:px-6 my-0.5 md:my-0 border-x-[1px] flex-1 text-center text-white">
+//         {durationLabel}
+//       </div>
+//       <div className="text-xxxs sm:text-xs md:text-sm lg:text-base pl-0.5 sm:pl-2 md:pl-4 lg:pl-6 pr-2 text-white flex flex-row items-center gap-0.5 sm:gap-2 md:gap-3">
+//         <div>{guestLabel}</div>
+//         <div className="p-0.5 sm:p-2 md:p-3 bg-soft-coral-500 rounded-full">
+//           <BiSearch className="w-2.5  md:w-5 lg:w-6 h-2.5 sm:h-4 md:h-5 lg:h-6" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Search;
